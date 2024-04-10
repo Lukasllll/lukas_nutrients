b86a33f39b144c0f36ed59572cbbfbd5cf5c0391
Simon <simon.kawa@gmx.de>
2024-04-10 18:55:43 +0200

    Made class abstract Mixin classes should be abstract

--- src/main/java/net/lukasllll/lukas_nutrients/mixin/BlockMixin.java
+++ src/main/java/net/lukasllll/lukas_nutrients/mixin/BlockMixin.java
@@ -6,7 +6,7 @@ import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(Block.class)
-public class BlockMixin implements INutrientPropertiesHaver {
+public abstract class BlockMixin implements INutrientPropertiesHaver {
     private NutrientProperties foodNutrientProperties;
 
     public NutrientProperties getFoodNutrientProperties() {
